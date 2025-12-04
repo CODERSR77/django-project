32e728ca9e933b862f8c798f25b1a63e39ae5674
CODERSR77 <rsrthecoder3333333@gmail.com>
2025-12-04 17:39:49 +0530

    added the ability to a and remove tasks

--- server/todo/urls.py
+++ server/todo/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home,name="TodoAppHome")
+    path('', views.home,name="TodoAppHome"),
+    path('add_task/', views.add_task,name="TodoAppAddTask"),
+    path('rm_task/<id>',views.remove_task,name="TodoAppRemoveTask")
+
 ]
--- server/todo/views.py
+++ server/todo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from .models import Task
 
@@ -6,3 +6,14 @@ from .models import Task
 def home(request):
     tasks = Task.objects.all()
     return render(request,"todo/index.html",{"tasks": tasks})
+def add_task(request):
+    title = request.POST.get("title")
+    description = request.POST.get("description")
+    if request.method == "POST":
+
+        tasks = Task.objects.create(Title=title,Description=description,Is_Done=False)
+    return redirect("/todo")
+def remove_task(request,id):
+    task = Task.objects.get(id=id)
+    task.delete()
+    return redirect("/todo")
