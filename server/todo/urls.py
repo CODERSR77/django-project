a9a3caf2de5afede5c67f4fa307a7bb83d726e00
CODERSR77 <rsrthecoder3333333@gmail.com>
2025-12-07 17:02:51 +0530

    Updated ui
    Made the completed tasks line through and mde checkbox hidden and also now we save changes to the db

--- server/todo/urls.py
+++ server/todo/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.home,name="TodoAppHome"),
     path('add_task/', views.add_task,name="TodoAppAddTask"),
-    path('rm_task/<id>',views.remove_task,name="TodoAppRemoveTask")
+    path('rm_task/<id>',views.remove_task,name="TodoAppRemoveTask"),
+    path('update_task_status/<id>',views.update_task_status,name="TodoAppUpdateTaskStatus")
 
 ]
--- server/todo/views.py
+++ server/todo/views.py
@@ -10,10 +10,14 @@ def add_task(request):
     title = request.POST.get("title")
     description = request.POST.get("description")
     if request.method == "POST":
-
         tasks = Task.objects.create(Title=title,Description=description,Is_Done=False)
     return redirect("/todo")
 def remove_task(request,id):
     task = Task.objects.get(id=id)
     task.delete()
+    return redirect("/todo")
+def update_task_status(request,id):
+    task = Task.objects.get(id=id)
+    task.Is_Done = not task.Is_Done
+    task.save()
     return redirect("/todo")
