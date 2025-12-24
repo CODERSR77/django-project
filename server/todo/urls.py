0a9cf5607fff11e531971c47f378c6fe3a44f9b6
CODERSR77 <rsrthecoder3333333@gmail.com>
2025-12-25 00:34:02 +0530

    Login and Logout
    Finally completed auth and the ability to login and logout modified the signup to redirect users to /todo after adding account showing logged user in the home page

--- server/todo/urls.py
+++ server/todo/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('add_task/', views.add_task,name="TodoAppAddTask"),
     path('rm_task/<id>',views.remove_task,name="TodoAppRemoveTask"),
     path('update_task_status/<id>',views.update_task_status,name="TodoAppUpdateTaskStatus"),
-    path('signup/',views.signup,name="TodoAppSignupPage")
+    path('signup/',views.signup_page,name="TodoAppSignupPage"),
+    path('login/',views.login_page,name="TodoAppLoginPage"),
+    path('logout/',views.logout_page,name="TodoAppLogoutPage")
 ]
--- server/todo/views.py
+++ server/todo/views.py
@@ -1,28 +1,34 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from django.contrib.auth.models import User
+from django.contrib.auth import login,logout,authenticate
+from django.contrib.auth.decorators import login_required
 from .models import Task
 
 # Create your views here.
+@login_required
 def home(request):
     tasks = Task.objects.all()
     return render(request,"todo/index.html",{"tasks": tasks})
+@login_required
 def add_task(request):
     title = request.POST.get("title")
     description = request.POST.get("description")
     if request.method == "POST":
         tasks = Task.objects.create(Title=title,Description=description,Is_Done=False)
     return redirect("/todo")
+@login_required
 def remove_task(request,id):
     task = Task.objects.get(id=id)
     task.delete()
     return redirect("/todo")
+@login_required
 def update_task_status(request,id):
     task = Task.objects.get(id=id)
     task.Is_Done = not task.Is_Done
     task.save()
     return redirect("/todo")
-def signup(request):
+def signup_page(request):
     if request.method == "POST":
         first_name = request.POST.get("First-Name")
         last_name = request.POST.get("Last-Name")
@@ -39,6 +45,24 @@ def signup(request):
                 user.first_name = first_name
                 user.last_name = last_name
                 user.save()
+                login(request,user=user)
+                return redirect("/todo")
                 
 
     return render(request,"todo/signup.html")
+def login_page(request):
+    if request.method == 'POST':
+        username = request.POST.get("Username")
+        password = request.POST.get("Password")
+        user = authenticate(request,username=username,password=password)
+        if user:
+            login(request,user=user)
+            return redirect('/todo')
+            print("Auth")
+        else:
+            print("NotAuth")
+
+    return render(request,'todo/login.html')
+def logout_page(request):
+    logout(request)
+    return redirect('/todo')
